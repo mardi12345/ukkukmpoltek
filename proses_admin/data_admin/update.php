8eed337f2a851d6024ad54978a0abc50de720233
mardi12345 <39816892+mardi12345@users.noreply.github.com>
2024-07-14 14:42:15 +0700

    deleting unnesecery enskripsi

--- proses_admin/data_admin/update.php
+++ proses_admin/data_admin/update.php
@@ -10,7 +10,7 @@ if (isset($_POST['update'])) {
     $email_users = $_POST['email_users'];
     $no_telp_users = $_POST['no_telp_users'];
     $username = $_POST['username'];
-    $password = md5($_POST['password']);
+    $password = $_POST['password'];
 
     $query = "UPDATE `tbl_users` SET `nama_users`='$nama_users',`alamat_users`='$alamat_users',`email_users`='$email_users',`no_telp_users`='$no_telp_users',`username`='$username',`password`='$password' WHERE  `id_users`='$id_users'";
     $result = mysqli_query($conn, $query);
