416f01cd3af41ce6d941c701acd43eebca09c7bc
mnurrasyid <nangnurr@gmail.com>
2024-07-15 15:19:14 +0700

    Menambah nama kepanjangan pataka

--- index.php
+++ index.php
@@ -71,6 +71,10 @@ $id_wali_murid = $_SESSION['id_wali_murid'];
                 <div class="layer-1-1 hidden-xs wow animate__slideInUp animate__animated" data-wow-duration="2s" data-wow-delay=".2s">
                   <h1 class="title1" style="color:white;" >UKK PATAKA</h1>
                 </div>
+                <div class="layer-1-1 hidden-xs wow animate__slideInUp animate__animated" data-wow-duration="2s" data-wow-delay=".2s">
+                  <h1 class="title1" style="color:white;" >(Pasukan Tanda Kehormatan)</h1>
+                </div>
+                
                 
                 <!-- layer 3 -->
                 <div class="layer-1-3 hidden-xs wow animate__slideInUp animate__animated" data-wow-duration="2s" data-wow-delay=".2s">
@@ -96,6 +100,7 @@ $id_wali_murid = $_SESSION['id_wali_murid'];
                 <div class="layer-1-1 hidden-xs wow animate__slideInUp animate__animated" data-wow-duration="2s" data-wow-delay=".2s">
                   <h1 class="title1" style="color:white;">UKK Drumband</h1>
                 </div>
+
 
                 <!-- layer 3 -->
                 <div class="layer-1-3 hidden-xs wow animate__slideInUp animate__animated" data-wow-duration="2s" data-wow-delay=".2s">
