1be9b5921957d7d0b27b0acb4b6ca158ce121246
mnurrasyid <nangnurr@gmail.com>
2024-07-17 09:02:55 +0700

    menambah ukm badminton

--- index.php
+++ index.php
@@ -246,6 +246,35 @@ $id_wali_murid = $_SESSION['id_wali_murid'];
     </div>
   </div><!-- End Slider -->
 
+  <!-- direction 7-->
+  <div id="slider-direction-8" class="slider-direction slider-two">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-12 col-sm-12 col-xs-12">
+              <div class="slider-content">
+                <!-- layer 1 -->
+                <div class="layer-1-1 hidden-xs wow animate__slideInUp animate__animated" data-wow-duration="2s" data-wow-delay=".2s">
+                  <h1 class="title1" style="color:white;">UKM BADMINTON</h1>
+                </div>
+                
+
+
+                <!-- layer 3 -->
+                <div class="layer-1-3 hidden-xs wow animate__slideInUp animate__animated" data-wow-duration="2s" data-wow-delay=".2s">
+                  <?php if( $id_wali_murid  == ''){?>
+
+                  <?php }else{ ?>
+                    <a class="ready-btn right-btn page-scroll" data-toggle="modal" data-target="#myModal" href="#services">Isi Quisioner</a>
+                  <?php }?>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div><!-- End Slider -->
+
   <main id="main">
 
     <!-- ======= About Section ======= -->
