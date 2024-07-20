17d0a5177c1d3fa7c20f2d74863b613c33a60e49
mardi12345 <39816892+mardi12345@users.noreply.github.com>
2024-07-20 17:04:26 +0700

    dat daftar

--- guru/data_konsultasi.php
+++ guru/data_konsultasi.php
@@ -15,7 +15,7 @@ $id_users = $_SESSION['id_users'];
 <div class="main-content">
   <section class="section">
     <div class="section-header">
-      <h1>Data Konsultasi</h1>
+      <h1>Data Pendaftar</h1>
     </div>
     <!-- <button type="button" class="btn btn-primary mb-2" data-toggle="modal" data-target="#tambahdata">
       Tambah Data
--- kepala_sekolah/data_konsultasi.php
+++ kepala_sekolah/data_konsultasi.php
@@ -15,7 +15,7 @@ $id_kategori = $_SESSION['id_kategori'];
 <div class="main-content">
   <section class="section">
     <div class="section-header">
-      <h1>Data Konsultasi</h1>
+      <h1>Data Pendaftar</h1>
     </div>
     <!-- <button type="button" class="btn btn-primary mb-2" data-toggle="modal" data-target="#tambahdata">
       Tambah Data
