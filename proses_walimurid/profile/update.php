09cf8819629715c9b062b30a0ecf2b47fcc722b5
mardi12345 <39816892+mardi12345@users.noreply.github.com>
2024-07-21 18:54:37 +0700

    fix bug update profil

--- proses_walimurid/profile/update.php
+++ proses_walimurid/profile/update.php
@@ -3,33 +3,54 @@
 include('../../include/koneksi.php');
 
 if (isset($_POST['update'])) {
-  
     $id_wali_murid = $_POST['id_wali_murid'];
-    $nama_ayah = $_POST['nama_ayah'];
-    $nama_ibu = $_POST['nama_ibu'];
-    $pekerjaan_ayah = $_POST['pekerjaan_ayah'];
-    $pekerjaan_ibu = $_POST['pekerjaan_ibu'];
-    $alamat = $_POST['alamat'];
-    $no_telp = $_POST['no_telp'];
-    $email = $_POST['email'];
-    $username = $_POST['username'];
-    $password = $_POST['password'];
+    $nama_ayah = mysqli_real_escape_string($conn, $_POST['nama_ayah']);
+    $nama_ibu = mysqli_real_escape_string($conn, $_POST['nama_ibu']);
+    $alamat = mysqli_real_escape_string($conn, $_POST['alamat']);
+    $no_telp = mysqli_real_escape_string($conn, $_POST['no_telp']);
+    $email = mysqli_real_escape_string($conn, $_POST['email']);
+    $username = mysqli_real_escape_string($conn, $_POST['username']);
+    $password = mysqli_real_escape_string($conn, $_POST['password']);
 
-    
-    $query = "UPDATE `tbl_wali_murid` SET `nama_ayah`='$nama_ayah',`nama_ibu`='$nama_ibu',`pekerjaan_ayah`='$pekerjaan_ayah',`pekerjaan_ibu`='$pekerjaan_ibu',`alamat`='$alamat',`no_telp`='$no_telp',`email`='$email',`username`='$username',`password`='$password' WHERE id_wali_murid = $id_wali_murid";
-    $result = mysqli_query($conn, $query);
+    // Query untuk mendapatkan id_murid dari tbl_wali_murid
+    $query_murid = "SELECT id_murid FROM tbl_wali_murid WHERE id_wali_murid = $id_wali_murid";
+    $result_murid = mysqli_query($conn, $query_murid);
 
-    if(!$result) {
-        die("Query Failed.");
+    if (!$result_murid) {
+        die("Query Failed: " . mysqli_error($conn));
+    }
+
+    $row_murid = mysqli_fetch_assoc($result_murid);
+    $id_murid = $row_murid['id_murid'];
+
+    // Update query untuk tbl_murid
+    $update_murid_query = "UPDATE `tbl_murid`
+                           SET `nama`='$nama_ayah', `nik`='$nama_ibu'
+                           WHERE `id_murid` = $id_murid";
+
+    $update_murid_result = mysqli_query($conn, $update_murid_query);
+
+    if (!$update_murid_result) {
+        die("Update Murid Query Failed: " . mysqli_error($conn));
+    }
+
+    // Update query untuk tbl_wali_murid
+    $update_wali_query = "UPDATE `tbl_wali_murid`
+                          SET `alamat`='$alamat',
+                              `no_telp`='$no_telp',
+                              `email`='$email',
+                              `username`='$username',
+                              `password`='$password'
+                          WHERE `id_wali_murid` = $id_wali_murid";
+
+    $update_wali_result = mysqli_query($conn, $update_wali_query);
+
+    if (!$update_wali_result) {
+        die("Update Wali Query Failed: " . mysqli_error($conn));
     }
 
     $_SESSION['message'] = 'Update Data Successfully';
     $_SESSION['message_type'] = 'success';
     header('Location: '.$base_url.'walimurid/profile.php');
-    
-    
-   
-   
 }
-
 ?>
